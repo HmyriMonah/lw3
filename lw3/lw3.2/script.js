936098157da63709680e5abf66b6ec6815d6d46c
HmyriMonah <sankoptelin@gmail.com>
2020-12-12 23:55:41 +0400

    fix 1

--- lw3/lw3.2/script.js
+++ lw3/lw3.2/script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
   var listingElements = ["apple", "orange"];
-  console.log(listingElements[0].length);
   var storeElements = [];
   updateUI();
   // логика JS, не связана с DOM
@@ -18,46 +17,35 @@ window.onload = function () {
       storeElements.splice(elementPosition, 1);
     }
   }
-
   // updateUI берет данные из массивов и занимается вставкой
   function updateUI() {
     var storeSelect = document.querySelector(".store-select");
     var listingSelect = document.querySelector(".listing-select");
     storeSelect.innerHTML = "";
     listingSelect.innerHTML = "";
-
     for (var i = 0; i < listingElements.length; i++) {
       var newOption = document.createElement("option");
       newOption.innerText = listingElements[i];
       listingSelect.append(newOption);
     }
-
     for (var i = 0; i < storeElements.length; i++) {
       var newOption = document.createElement("option");
       newOption.innerText = storeElements[i];
       storeSelect.append(newOption);
     }
   }
-  // регистрируем события
-  var addButton = document.querySelector("#add-button");
-  var reAddButton = document.querySelector("#re-add-button");
-  var deleteButton = document.querySelector("#delete-button");
-  var newElementButton = document.querySelector("#new-element-button");
-  var sortButton = document.querySelector("#sort-button");
-  var renameButton = document.querySelector("#rename-button");
-  addButton.onclick = function () {
-    var selectedOption = document.querySelector(
-      ".listing-select option:checked"
-    );
-    addToStoreElements(selectedOption.innerText);
-    updateUI();
-  };
-  reAddButton.onclick = function () {
-    var selectedOption = document.querySelector(".store-select option:checked");
-    addToListingElements(selectedOption.innerText);
-    updateUI();
-  };
-  deleteButton.onclick = function () {
+  function sort() {
+    if (listingElements[0] > listingElements[1]) {
+      listingElements = listingElements.sort();
+      storeElements = storeElements.sort();
+    } else {
+      listingElements = listingElements.sort();
+      storeElements = storeElements.sort();
+      listingElements = listingElements.reverse();
+      storeElements = storeElements.reverse();
+    }
+  }
+  function remove() {
     if (document.querySelector(".store-select option:checked") !== null) {
       var selectedOption = document.querySelector(
         ".store-select option:checked"
@@ -72,6 +60,49 @@ window.onload = function () {
         1
       );
     }
+  }
+  function rename(name) {
+    if (document.querySelector(".store-select option:checked") !== null) {
+      var selectedOption = document.querySelector(
+        ".store-select option:checked"
+      );
+      storeElements.splice(
+        storeElements.indexOf(selectedOption.innerText),
+        1,
+        name
+      );
+    } else {
+      var selectedOption = document.querySelector(
+        ".listing-select option:checked"
+      );
+      listingElements.splice(
+        listingElements.indexOf(selectedOption.innerText),
+        1,
+        name
+      );
+    }
+  }
+  // регистрируем события
+  var addButton = document.querySelector("#add-button");
+  var AddToListingButton = document.querySelector("#add-listing-button");
+  var deleteButton = document.querySelector("#delete-button");
+  var newElementButton = document.querySelector("#new-element-button");
+  var sortButton = document.querySelector("#sort-button");
+  var renameButton = document.querySelector("#rename-button");
+  addButton.onclick = function () {
+    var selectedOption = document.querySelector(
+      ".listing-select option:checked"
+    );
+    addToStoreElements(selectedOption.innerText);
+    updateUI();
+  };
+  AddToListingButton.onclick = function () {
+    var selectedOption = document.querySelector(".store-select option:checked");
+    addToListingElements(selectedOption.innerText);
+    updateUI();
+  };
+  deleteButton.onclick = function () {
+    remove();
     updateUI();
   };
   newElementButton.onclick = function () {
@@ -79,34 +110,13 @@ window.onload = function () {
     listingElements.push(newElement);
     updateUI();
   };
-  var key = true;
   sortButton.onclick = function () {
-    if (key) {
-      listingElements = listingElements.sort();
-      storeElements = storeElements.sort();
-      key = false;
-    } else {
-      listingElements = listingElements.sort();
-      storeElements = storeElements.sort();
-      listingElements = listingElements.reverse();
-      storeElements = storeElements.reverse();
-      key = true;
-    }
+    sort();
     updateUI();
   };
   renameButton.onclick = function () {
     var newName = prompt("Enter new name");
-    if (document.querySelector(".store-select option:checked") !== null) {
-      var selectedOption = document.querySelector(
-        ".store-select option:checked"
-      );
-      storeElements.splice(storeElements.indexOf(selectedOption.innerText), 1,newName);
-    } else {
-      var selectedOption = document.querySelector(
-        ".listing-select option:checked"
-      );
-      listingElements.splice(listingElements.indexOf(selectedOption.innerText), 1,newName);
-    }
+    rename(newName);
     updateUI();
   };
 };
